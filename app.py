0d69ea8f24ea1795df59bb464849fb322c9079d0
Ayoola <johnsonayoolaojo@gmail.com>
2025-02-04 18:45:47 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+print("Hello World")
+
+if 6 < 9:
+    print("6 is lesser than 9")
+
+    name = "Ola"
+
+
+    print(name)
+
+    #This is a greeting message
+    print("good afternoon")
+
+x = 89
+y ="Olalekan"
+
+print(x)
+print(y)
+
+z = float(3)
+print()
+
+print(z)
+
+
+a = 9
+b = 6.7
+c = "Ola"
+d = True 
+e = []
+f = {}
+g = ()
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(float))
+print(type(g))
