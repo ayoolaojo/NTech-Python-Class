f74ccaebd2fd18f46e9a400b9ef78ae6e870d691
Ayoola <johnsonayoolaojo@gmail.com>
2025-02-07 14:57:03 +0100

    file updated

---
+++ firstclass.py
@@ -0,0 +1,98 @@
+# print("Hello World")
+
+# if 6 < 9:
+#     print("6 is lesser than 9")
+
+#     name = "Ola"
+
+
+#     print(name)
+
+#     #This is a greeting message
+#     print("good afternoon")
+
+# x = 89
+# y ="Olalekan"
+
+# print(x)
+# print(y)
+
+# z = float(3)
+# print()
+
+# print(z)
+
+
+# a = 9
+# b = 6.7
+# c = "Ola"
+# d = True 
+# e = []
+# f = {}
+# g = ()
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(float))
+# print(type(g))
+
+
+
+
+# name = 'Ayoola'
+# age = 20
+# print("My name is",name,"I am", age, "years old")
+# print(f"My name is {name} and I am {age} years old")
+
+# firstname = input("what is your name")
+# print(firstname)
+
+# age = input("what's your age")
+
+# print(f"My name is {name} and I am {age} years old")
+
+
+# myName = input("what's your name")
+# YoB = input("input your year of birth")
+# currentAge = 2025-int(YoB)
+
+# print(f"My name is {myName} and I am {currentAge} years old")
+
+# gramme = input ("what's your weight in grammes")
+# weightInKg = int(gramme)/ 1000
+# print(f"you weigh {weightInKg} kg")
+
+
+# Mname = "olalekan"
+# Mname.upper()
+# print (Mname[0:2])
+
+# print(Mname[-3: ])
+
+# print(len(Mname))
+
+# print (Mname.upper())
+
+# print(Mname.capitalize())
+
+text = "This is a text, and this text is for example"
+print(text.count("i"))
+print(text.count("t"))
+print(text.count("this"))
+
+texts = """ 
+My name is Ola
+I am 23 years old
+I am from NYC
+"""
+
+print(texts)
+
+hello = " hello,   world   !"
+
+print(hello.strip())
+
+print(hello.replace("world", "Ntech"))
